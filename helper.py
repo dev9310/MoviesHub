54559b9e30a3d051407a35886e45b2e36f442be6
Dev Shakeywar <122546955+bannu82@users.noreply.github.com>
2024-09-20 18:09:00 +0530

    commit

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ def get_db_connection():
 
 def fetch_movie(n=30):
     conn = get_db_connection()
-    df = pd.read_sql(f"SELECT * FROM 'movies' WHERE Time=2024 LIMIT {n}", conn)
+    df = pd.read_sql(f"SELECT * FROM 'movies' WHERE Time=2024 ORDER BY RANDOM() LIMIT {n}", conn)
     conn.close()
     data = df.to_dict(orient='records')
     return data
