7e97ab6fba56b40209cc2204c81cea1daf343ae9
Dev Shakeywar <122546955+bannu82@users.noreply.github.com>
2024-09-24 22:53:09 +0530

    commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
-import helper
+from helper import Db_Manager as db_mg
 
 app = Flask(__name__)
+db = db_mg()  
 
 @app.route('/')
 def welcome():
@@ -9,11 +10,21 @@ def welcome():
 
 @app.route('/home')
 def home():
-    movies = helper.fetch_movie()
-    total_movies= helper.count_movies()
+    pg_no=1
+    # Create a new instance for each request
+    ttl_pg = db.total_pages
+    movies = db.fetch_by_page_no(pg_no)
+    # db.close_connection()  # Close the connection after use
     
-    return render_template('home.html', movies=movies ,total_movies=total_movies)
+    return render_template('home.html', movies=movies ,pg_no=pg_no , total_pages =ttl_pg )
+
+@app.route('/home/page=<int:pg_no>')
+def pages(pg_no):
+    movies = db.fetch_by_page_no(pg_no)
+    ttl_mv = db.total_movies
+    ttl_pg = db.total_pages
     
+    return render_template('home.html', pg_no=pg_no , movies=movies , total_movies=ttl_mv , total_pages =ttl_pg )
 
 if __name__ == "__main__":
     app.run(debug=True)
--- helper.py
+++ helper.py
@@ -1,23 +1,50 @@
 import sqlite3
 import pandas as pd
-def get_db_connection():
-    conn = sqlite3.connect('data.db')  # Connect to the SQLite database
-    conn.row_factory = sqlite3.Row  # To access rows as dictionaries
-    return conn
 
-def fetch_movie(n=30):
-    conn = get_db_connection()
-    df = pd.read_sql(f"SELECT * FROM 'movies' WHERE Time=2024 LIMIT {n}", conn)
-    conn.close()
-    data = df.to_dict(orient='records')
-    return data
-
-# ORDER BY RANDOM()
-
-def count_movies():
+class Db_Manager:
     
-    conn = get_db_connection()
-    query = f"SELECT COUNT(*) AS total_movies FROM 'movies'"
-    result = pd.read_sql(query, conn)
-    conn.close()
-    return result['total_movies'][0]
+    def __init__(self):
+        self.total_movies = 0 
+        self.conn = None
+        self.count_movies()
+        self.total_pages = self.total_movies//30 
+    
+    def get_db_connection(self):
+        # Set check_same_thread=False to allow usage across threads
+        self.conn = sqlite3.connect('data.db', check_same_thread=False)
+        self.conn.row_factory = sqlite3.Row
+        
+    def fetch_movie(self, n=30):
+        if self.conn is None:
+            self.get_db_connection()
+        
+        # Fetch movies with a limit
+        df = pd.read_sql(f"SELECT * FROM 'movies' WHERE Time = 2024 LIMIT {n}", self.conn)
+        data = df.to_dict(orient='records')
+        return data
+
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+                
+    def count_movies(self):
+        if self.conn == None:
+            self.get_db_connection()
+            
+        query = f"SELECT COUNT(*) AS total_movies FROM 'movies'"
+        result = pd.read_sql(query,self.conn)
+        self.total_movies = result['total_movies'][0]     
+        
+    
+    def fetch_by_page_no(self, pg_no=1):
+        if self.conn is None:
+            self.get_db_connection()
+        
+        # Adjust LIMIT and OFFSET for pagination
+        offset = (pg_no - 1) * 30  # Calculate the offset based on the page number
+        query = f"SELECT * FROM 'movies' LIMIT 30 OFFSET {offset}"
+        
+        df = pd.read_sql(query, self.conn)
+        data = df.to_dict(orient='records')
+        return data
